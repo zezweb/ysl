cab7da542bae163f22b4ef303948ab247d1be1af
zezweb <86402299+zezweb@users.noreply.github.com>
2021-08-02 18:01:30 +0900

    Create buy_main_5.js

---
+++ js/buy_main_5.js
@@ -0,0 +1,25 @@
+function init(){
+
+    const buySet = document.querySelector('.buyset');
+    var buyNum = document.querySelector('.buy_box_1 > p:nth-child(2)');
+    var plus = document.querySelector('.buy_box_1 > p:nth-child(3)');
+    var minus = document.querySelector('.buy_box_1 > p:nth-child(1)');
+    var bleen = true;
+    var i = 0;
+        
+
+    plus.addEventListener('click',function(){
+            if(bleen){
+                buyNum.textContent = i++
+            }
+    });
+
+    minus.addEventListener('click',function(){
+            if(bleen, i>0){
+                buyNum.textContent = i--
+            }
+    });
+
+}
+    
+window.onload = init;
