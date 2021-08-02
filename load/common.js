127a8c1b1621a36725d317c95f17e498b89e86d1
zezweb <86402299+zezweb@users.noreply.github.com>
2021-08-02 18:01:33 +0900

    Update common.js

--- load/common.js
+++ load/common.js
@@ -8,5 +8,16 @@ $(function(){
         $('header nav a').on('mouseenter',function(){
             $(this).css('color','black');
         });
+
+        const elBurger = document.querySelector('.menu-trigger');
+        const elNav = document.querySelector('.h_menu nav');
+        elBurger.addEventListener('click',function(){
+            // console.log('a')
+            this.classList.toggle('active-7');
+            elNav.classList.toggle('active');
+        });
+
+
+
     }
 });
